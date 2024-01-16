ca7f651012749620713b2ca2acbe1ec8fa885850
jav <javierlopeztl1@gmail.com>
2024-01-16 12:40:00 +0100

    C++: Implementación de algoritmos de ordenamiento (Método Burbuja) y lógica de intercambio de valores

---
+++ 016.0-PROGRAMA-MENOR-O-MAYOR-18.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+int main(int argc, char){
+
+ int numero;
+    cout<<"ingrese su edad: ";
+    cin>>numero;
+    if(numero>=18){
+
+        cout<<"usted e mayor de edad"<<endl;
+    }else{cout<<"usted no es mayor de edad"<<endl;}
+    return 0;
+}
