234ae44eae8e3a1a84e86cecb0894dd57ca428fd
jav <javierlopeztl1@gmail.com>
2024-01-19 14:50:00 +0100

    C++: Implementación de alias de tipos mediante typedef para mejorar la legibilidad y semántica del código

---
+++ 019.0-typedef.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+
+typedef int  arreglo10[10];
+
+int main(int argc, char){
+
+    arreglo10 a[1,2,3,4,5,6,7,8,9];
+    arreglo10 b[1,4,57,9];
+    arreglo10 c[2,4,8,16];
+
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    return 0;
+}
