8c25f72595ac10fc3992151fb9c5fa32390f1359
jav <javierlopeztl1@gmail.com>
2024-03-20 15:00:00 +0100

    JS (078): Estructuras de datos mediante objetos literales y métodos

---
+++ 078-objetos.js
@@ -0,0 +1,18 @@
+// ejercicios.js
+
+// Ejercicio:
+// Crea un objeto que represente a un usuario.
+// Debe tener nombre, edad y ciudad.
+// Después muestra sus datos en consola.
+
+// Solución:
+
+const usuario = {
+    nombre: "Lucía",
+    edad: 25,
+    ciudad: "Cádiz"
+};
+
+console.log(`Nombre: ${usuario.nombre}`);
+console.log(`Edad: ${usuario.edad}`);
+console.log(`Ciudad: ${usuario.ciudad}`);
