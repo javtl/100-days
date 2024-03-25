f65a4724eb9fffd92f2741cd7544ba9e13277c54
jav <javierlopeztl1@gmail.com>
2024-03-25 10:00:00 +0100

    lost exercises+ re-oganized structure

---
+++ 01-cpp-fundamentals/Poner-Ñ_detectada.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(){
+	
+	 setlocale(LC_CTYPE,"Spanish"); 
+	
+	cout<<"España"<<endl;
+	
+	system("PAUSE");
+	return 0;
+}
---
+++ 01-cpp-fundamentals/cadenas.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+#include <string.h>
+using namespace std;
+
+int main(){
+	char nombre[]="javier";
+	char nombre2[11] = {'j','a','v','i','e','r',};
+	
+	cout<<nombre<<endl;
+	cout<<nombre2<<endl;
+	
+	getch();
+	return 0;
+}
